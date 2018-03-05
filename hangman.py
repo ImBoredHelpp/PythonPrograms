7348d3b5cc7fb468f5143ee502577cbc8deb3843
Aaron <36903060+ImBoredHelpp@users.noreply.github.com>
2018-03-04 17:36:47 -0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -15,23 +15,26 @@ import time
 from threading import Thread
 import os
 
+words_guessed = []
 letters_guessed = ""
 incorrect = 0
 guess_word = ""
 guess_letter = ""
 
 # "dead" checks if the guesser ran out of guesses
-# or if the guesser ran out of time.
+# or if the guesser guessed the word. Once "dead"
+# is True, it stops the timer thread.
 dead = False
 
 # Here, a player enters a word to be guessed.
 # The code below checks if the word given has
 # a space or 1 or less characters.
 
-word = input("Player 1, please enter a word to be guessed: ")
-while len(word) <= 1 or " " in word:
+word = input("Player 1, please enter a word to be guessed: ").upper()
+while len(word) <= 1 or not word.isalpha():
     print("Please enter a valid word!")
-    word = input("Player 1, please enter a word to be guessed: ")
+    print("")
+    word = input("Player 1, please enter a word to be guessed: ").upper()
 word_length = "*" * len(word)
 
 while True:
@@ -72,18 +75,17 @@ def ready_set_go():
 # This is the main timer for the program
 def countdown():
     global timer, incorrect, dead, word
-    while (timer != 0) and (not dead):
-        for i in range(timer):
-            if dead:
-                break
-            else:
-                time.sleep(1)
-                timer = timer - 1
-                if timer == 0:
-                    print("")
-                    print("")
-                    print("Time's up! You lose!")
-                    print("The word was: " + word.upper())
+    for i in range(timer):
+        if dead:
+            break
+        else:
+            time.sleep(1)
+            timer = timer - 1
+            if timer == 0:
+                print("")
+                print("")
+                print("Time's up! You lose!")
+                print("The word was: " + word)
     # This immediately exits the program. I would use
     # "sys.exit()" but that only exits out of a thread.
     # I want to kill the whole program.
@@ -99,44 +101,90 @@ def hangman():
             print("You have " + str(timer) + " seconds left!")
         # Here, a user enters a letter or word as their guess.
         guess = input("Player 2, please enter a letter or word guess: ")
-        if dead:
-            break
+        while not guess.isalpha():
+            print("Please enter a valid guess!")
+            print("")
+            print(word_length)
+            print("")
+            print("You have " + str(timer) + " seconds left!")
+            guess = input("Player 2, please enter a letter or word guess: ")
         if len(guess) == 1:
-            # The code below makes sure the guesser doesn't guess the
-            # same wrong or right letter twice.
-            guess_letter = guess
-            for i in range(len(letters_guessed.upper())):
-                if dead:
-                    break
-                while ((guess_letter.upper() == letters_guessed[i].upper()) or
-                       guess_letter.upper() in letters_guessed.upper()) and (not letters_guessed == "") and (not dead):
-                    print("You already guessed this!")
+            guess_letter = guess.upper()
+            # The code below makes sure the guesser doesn't
+            # guess the same wrong or right letter twice.
+            while guess_letter in letters_guessed:
+                print("You already guessed this!")
+                print("")
+                print(word_length)
+                print("")
+                print("You have " + str(timer) + " seconds left!")
+                guess = input("Player 2, please enter a letter or word guess: ")
+                while not guess.isalpha():
+                    print("Please enter a valid guess!")
                     print("")
                     print(word_length)
                     print("")
                     print("You have " + str(timer) + " seconds left!")
                     guess = input("Player 2, please enter a letter or word guess: ")
-                    if dead:
-                        break
-                    # Lines 124-127 check to see if the user puts in a word after
-                    # guessing the same letter twice, right or wrong. This was
-                    # previously a bug, but now squashed.
-                    if len(guess) > 1:
-                        guess_word = guess
-                        guess_letter = ""
-                        break
-                    else:
-                        guess_letter = guess
-                break
+                # The four lines below check to see if the
+                # user puts in a word after guessing letters.
+                if len(guess) > 1:
+                    guess_word = guess.upper()
+                    guess_letter = ""
+                    break
+                else:
+                    guess_letter = guess.upper()
+                guess_word = ""
             letters_guessed = letters_guessed + guess_letter
-            guess_word = ""
-        if dead:
-            break
         if len(guess) > 1:
-            guess_word = guess
-            guess_letter = ""
-        if ((guess_word.upper() != word.upper()) and
-                (not guess_letter.upper() in word.upper() or guess_letter.upper() == "")):
+            guess_word = guess.upper()
+            # The code below makes sure the guesser
+            # doesn't guess the same wrong word twice.
+            while guess_word in words_guessed:
+                print("You already guessed this!")
+                print("")
+                print(word_length)
+                print("")
+                print("You have " + str(timer) + " seconds left!")
+                guess = input("Player 2, please enter a letter or word guess: ")
+                while not guess.isalpha():
+                    print("Please enter a valid guess!")
+                    print("")
+                    print(word_length)
+                    print("")
+                    print("You have " + str(timer) + " seconds left!")
+                    guess = input("Player 2, please enter a letter or word guess: ")
+                if len(guess) == 1:
+                    guess_letter = guess.upper()
+                    guess_word = ""
+                    while guess_letter in letters_guessed:
+                        print("You already guessed this!")
+                        print("")
+                        print(word_length)
+                        print("")
+                        print("You have " + str(timer) + " seconds left!")
+                        guess = input("Player 2, please enter a letter or word guess: ")
+                        while not guess.isalpha():
+                            print("Please enter a valid guess!")
+                            print("")
+                            print(word_length)
+                            print("")
+                            print("You have " + str(timer) + " seconds left!")
+                            guess = input("Player 2, please enter a letter or word guess: ")
+                        if len(guess) > 1:
+                            guess_word = guess.upper()
+                            guess_letter = ""
+                            break
+                        else:
+                            guess_letter = guess.upper()
+                            guess_word = ""
+                    letters_guessed = letters_guessed + guess_letter
+                else:
+                    guess_word = guess.upper()
+                    guess_letter = ""
+            if guess_word != "":
+                words_guessed.append(guess_word)
+        if (guess_word != word) and ((guess_letter not in word) or (guess_letter == "")):
             # The code below counts how many times the guesser
             # guessed something wrong. Once the user guesses wrong 7
             # times, the game ends and the guesser loses.
@@ -237,7 +285,7 @@ def hangman():
                 print("")
                 print(word_length)
                 print("")
-            elif incorrect == 7 and timer != 0:
+            elif incorrect == 7:
                 print("You didn't guess the word! You lose!")
                 print("")
                 print("      ________")
@@ -253,29 +301,29 @@ def hangman():
                 print("")
                 print(word_length)
                 print("")
-                print("The word was: " + word.upper())
+                print("The word was: " + word)
                 dead = True
                 break
         else:
             # This code congratulates the guesser if they guessed a word
             # and got the word right.
-            if guess_word.upper() == word.upper():
+            if guess_word == word:
                 print("You guessed the word! You win!")
                 dead = True
                 break
             # This code checks if the letter the guesser guessed a letter
             # that was in the word right. If they did, it replaces it with
             # the "*"
-            elif guess_letter.upper() in word.upper():
-                for i in range(len(word.upper())):
-                    if guess_letter.upper() == word[i].upper() and word_length[i] == "*":
-                        word_length = word_length[:i] + guess_letter.upper() + word_length[i + 1:]
+            elif guess_letter in word:
+                for i in range(len(word)):
+                    if guess_letter == word[i] and word_length[i] == "*":
+                        word_length = word_length[:i] + guess_letter + word_length[i + 1:]
                 print("")
                 print(word_length)
                 print("")
                 # This code congratulates the guesser if they guessed
                 # the word after entering in letters as guesses.
-                if word_length == word.upper():
+                if word_length == word:
                     print("You guessed the word! You win!")
                     dead = True
                     break
